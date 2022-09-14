e2cc003fa6eaeb60cb359355e08dc4a41f4b9c62
Sebastien Villar <seb@height.app>
2022-09-14 11:53:12 +0200

    Improve todoRegexes typing

--- src/todoRegexes.ts
+++ src/todoRegexes.ts
@@ -2,31 +2,51 @@ import commentPatterns from '@heightapp/comment-patterns';
 import {commentRegexesFromPatterns} from 'helpers/commentRegexes';
 import {multiLineTodoRegex, singleLineTodoRegex} from 'helpers/todoRegex';
 
-const todoRegexes = (withMatch: boolean) => {
-  const allPatterns = commentPatterns.allPatterns();
-  return allPatterns.map((pattern) => {
-    const commentRegexes = commentRegexesFromPatterns(pattern);
-    const singleLine = (() => {
-      if (withMatch) {
-        return commentRegexes.singleLine?.length ? singleLineTodoRegex(true, ...commentRegexes.singleLine) : undefined;
-      }
-      return commentRegexes.singleLine?.length ? singleLineTodoRegex(false, ...commentRegexes.singleLine) : undefined;
-    })();
+type WithMatchResult = {
+  name: string;
+  nameMatchers: Array<string>;
+  singleLine: {regex: RegExp; prefixCapture: number; nameCapture: number} | undefined;
+  multiLine: Array<{regex: RegExp; prefixCapture: number; nameCapture: number; suffixCapture: number}> | undefined;
+};
 
-    const multiLine = (() => {
-      if (withMatch) {
-        return commentRegexes.multiLine?.length ? commentRegexes.multiLine.map(({start, end}) => multiLineTodoRegex(true, start, end)) : undefined;
-      }
-      return commentRegexes.multiLine?.length ? commentRegexes.multiLine.map(({start, end}) => multiLineTodoRegex(false, start, end)) : undefined;
-    })();
+type WithoutMatchResult = {
+  name: string;
+  nameMatchers: Array<string>;
+  singleLine: RegExp | undefined;
+  multiLine: Array<RegExp> | undefined;
+};
 
+function todoRegex(withMatch: true, pattern: ReturnType<typeof commentPatterns.allPatterns>[0]): WithMatchResult;
+function todoRegex(withMatch: false, pattern: ReturnType<typeof commentPatterns.allPatterns>[0]): WithoutMatchResult;
+function todoRegex(withMatch: boolean, pattern: ReturnType<typeof commentPatterns.allPatterns>[0]) {
+  const commentRegexes = commentRegexesFromPatterns(pattern);
+
+  if (withMatch) {
+    const singleLine = commentRegexes.singleLine?.length ? singleLineTodoRegex(withMatch, ...commentRegexes.singleLine) : undefined;
+    const multiLine = commentRegexes.multiLine?.length ? commentRegexes.multiLine.map(({start, end}) => multiLineTodoRegex(withMatch, start, end)) : undefined;
     return {
       name: pattern.name,
       nameMatchers: pattern.nameMatchers,
       singleLine,
       multiLine,
     };
-  });
-};
+  }
+
+  const singleLine = commentRegexes.singleLine?.length ? singleLineTodoRegex(withMatch, ...commentRegexes.singleLine) : undefined;
+  const multiLine = commentRegexes.multiLine?.length ? commentRegexes.multiLine.map(({start, end}) => multiLineTodoRegex(withMatch, start, end)) : undefined;
+  return {
+    name: pattern.name,
+    nameMatchers: pattern.nameMatchers,
+    singleLine,
+    multiLine,
+  };
+}
+
+function todoRegexes(withMatch: true): Array<WithMatchResult>;
+function todoRegexes(withMatch: false): Array<WithoutMatchResult>;
+function todoRegexes(withMatch: boolean) {
+  const allPatterns = commentPatterns.allPatterns();
+  return withMatch ? allPatterns.map((pattern) => todoRegex(withMatch, pattern)) : allPatterns.map((pattern) => todoRegex(withMatch, pattern));
+}
 
 export default todoRegexes;
